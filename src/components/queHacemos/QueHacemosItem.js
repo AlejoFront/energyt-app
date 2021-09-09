139c8e3de347edcb3a54ddb3250d2a394dd49715
AlejoFront <alejandro9425@gmail.com>
2021-09-09 09:24:51 -0500

    Detalles Finales

--- src/components/queHacemos/QueHacemosItem.js
+++ src/components/queHacemos/QueHacemosItem.js
@@ -5,7 +5,7 @@ const QueHacemosItem = ({date}) => {
     return (
         <div className="card__item">
             <figure className="card__img">
-                <img src={ICON_QUE_HACEMOS} alt='icono' className="card__picture" />
+                <img src={date.icono || ICON_QUE_HACEMOS} alt='icono' className="card__picture" />
             </figure>
             <h3 className="card__title">{date.nombre}</h3>
             <article className="card__paragraph"> {ReactHtmlParser(date.descripcion)} </article>
